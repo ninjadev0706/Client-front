7f4cc30b1c5ed7140301950d79574aa98e19d7ed
JamesHiro <unicornjameser@gmail.com>
2022-10-08 11:03:34 +0900

    server change

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:5000";
-// export const API_URL = "https://spade-client-server.herokuapp.com"
+// export const API_URL = "http://localhost:5000";
+export const API_URL = "https://spade-client-server.herokuapp.com"
